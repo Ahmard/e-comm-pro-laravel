be5e172a18131e6c1d60ea81a820600eaa2fbff5
maimalee <anasment6@gmail.com>
2021-03-25 11:50:37 -0700

    header, footer, login and product page

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('products')->insert([
+            [
+                'name'=> 'Royal',
+                'price'=>"15000",
+                'description'=> "Royal Sofa with high good quality",
+                'category'=> 'chair',
+                'gallery'=> 'C:\Users\ANAS MAIMALEE\Desktop\L\e-comm-pro\resources\public\images\IMG-20210216-WA0001.jpg', 
+            ],
+            [
+                'name'=> 'Tv Stand',
+                'price'=>"50000",
+                'description'=> "Tv stand with high good quality",
+                'category'=> 'tv-Stand',
+                'gallery'=> 'C:\Users\ANAS MAIMALEE\Desktop\L\e-comm-pro\resources\public\images\IMG-20210216-WA0002.jpg', 
+            ],
+            [
+                'name'=> 'Royal',
+                'price'=>"150000",
+                'description'=> "Royal Sofa with high good quality",
+                'category'=> 'chair',
+                'gallery'=> 'C:\Users\ANAS MAIMALEE\Desktop\L\e-comm-pro\resources\public\images\IMG-20210216-WA0004~2.jpg', 
+            ],
+            [
+                'name'=> 'Royal',
+                'price'=>"150000",
+                'description'=> "Royal Sofa with high good quality",
+                'category'=> 'chair',
+                'gallery'=> 'C:\Users\ANAS MAIMALEE\Desktop\L\e-comm-pro\resources\public\images\IMG-20210216-WA0003.jpg', 
+            ],
+            [
+                'name'=> 'Royal',
+                'price'=>"150000",
+                'description'=> "Royal Sofa with high good quality",
+                'category'=> 'chair',
+                'gallery'=> 'C:\Users\ANAS MAIMALEE\Desktop\L\e-comm-pro\resources\public\images\IMG-20210216-WA0006.jpg', 
+            ]
+        ]);
+    }
+}
---
+++ resources/views/product.blade.php
@@ -0,0 +1,60 @@
+@extends('master')
+@section('content')
+
+
+<div class="container custom-product">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+      
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+
+            @foreach ($products as $item)
+            <div class="item {{ $item->id==1? 'active': ''}}">
+                <img src="{{ $item->gallery }}" alt="fuck">
+                <div class="carousel-caption">
+                  <h3>{{ $item->name }}</h3>
+                  <p>{{ $item->description }}</p>
+                  
+                </div>
+              </div>
+            </div>
+            @endforeach
+
+            
+        
+      
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+</div>
+
+            @foreach ($products as $item)
+              <div class="container pro-style" >
+                <table border="1">
+                  <tr><td>Product ID: </td><td>{{ $item->id }}</td></tr>
+                  <tr><td>Product Name: </td><td>{{ $item->name }}</td></tr>
+                  <tr><td>Product Price: </td><td>{{ $item->price }}</td></tr>
+                  <tr><td>Product Description: </td><td>{{ $item->description }}</td></tr>
+                  <tr><td>Product ID: </td><td>{{ $item->category }}</td></tr>
+                  <tr><td>Product ID: </td><td><img src="{{ $item->gallery }}" alt="error"></td></tr>
+                </table>
+              </div>
+                
+            @endforeach
+         
+@endsection
+
+
